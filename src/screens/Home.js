a2603385bcbcdb5368f59b554b26baf4152bf72d
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-03-04 02:08:30 -0600

    Starting Search Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -54,24 +54,30 @@ export default class HomeScreen extends React.Component {
     }
 
     render() {
+        const { navigation } = this.props
         return (
+        
         <View style={{flex: 1, backgroundColor:'#151515'}}>
             <Card transparent>
-                <CardItem style={{backgroundColor:'#333333'}}>
-                    <Left>
-                        <Thumbnail square source={{uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/The_Pretty_Reckless_-_Going_To_Hell_%28Official_Album_Cover%29.png/220px-The_Pretty_Reckless_-_Going_To_Hell_%28Official_Album_Cover%29.png'}} />
-                    </Left>
-                    <Body>
-                        <Text style={{color:'white'}}>NativeBase</Text>
-                        <Text note style={{color:'#9F9F9F'}}>GeekyAnts</Text>
-                    </Body>
-                    <TouchableOpacity onPress={() => {this.likedSong()}}>
-                        <Icon active name={this.state.heart} style={{color: this.state.heartcolor}} />
-                    </TouchableOpacity> 
-                    <TouchableOpacity onPress={() => {this.pauseSong()}}>
-                        <Icon active name={this.state.pausePlay} style={{color:'white'}} />
-                    </TouchableOpacity> 
-                </CardItem>
+                <TouchableOpacity onPress={()=>{
+                    navigation.navigate('Search')
+                }}>
+                    <CardItem style={{backgroundColor:'#333333'}}>
+                        <Left>
+                            <Thumbnail square source={{uri: 'https://www.spirit-of-metal.com/les%20goupes/T/The%20Pretty%20Reckless/Kill%20Me/Kill%20Me_2513.jpg'}} />
+                        </Left>
+                        <Body>
+                            <Text style={{color:'white'}}>Track</Text>
+                            <Text note style={{color:'#9F9F9F'}}>Autor</Text>
+                        </Body>
+                        <TouchableOpacity onPress={() => {this.likedSong()}}>
+                            <Icon active name={this.state.heart} style={{color: this.state.heartcolor}} />
+                        </TouchableOpacity> 
+                        <TouchableOpacity onPress={() => {this.pauseSong()}}>
+                            <Icon active name={this.state.pausePlay} style={{color:'white'}} />
+                        </TouchableOpacity> 
+                    </CardItem>
+                </TouchableOpacity>
             </Card>    
         </View>
         
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { View, Text, Button} from 'react-native'
+import { View, ScrollView } from 'react-native'
+import { Item, Input, Icon, Button, Text } from 'native-base';
 
 export default class SearchScreen extends React.Component {  
     render() {
         return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button
-                title="Search"
-            />
+        <View style={{flex: 1, backgroundColor:'#151515'}}>
+            <View style={{margin:25}}>
+                <Text style={{color:'white', fontSize:28, fontWeight:'bold'}}>Search</Text>
+            </View>
+            <Item rounded style={{backgroundColor:'white', width:'92%', alignSelf:'center'}}>
+                <Icon name="ios-search-outline" />
+                <Input 
+                    placeholder="Artists, songs, or podcasts" 
+                    placeholderTextColor={'black'} 
+                    style={{fontWeight:'bold', fontSize:16}}
+                />
+            </Item>
         </View>
         )
     }
